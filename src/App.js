bc278c20588200b51f881c832e23fd491f5a05aa
bmirt <mircxu1898@gmail.com>
2019-05-23 17:07:48 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { changeTitle } from "./actions/Actions";
-
+import React from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Todos from "./containers/Todos";
 import "./App.css";
 
-class App extends Component {
-  changeTitleFunction = id => {
-    let newTitle = prompt("please put new title");
-    let ourObject = this.props.todos.find(todo => {
-      return todo.id === id;
-    });
-    ourObject.title = newTitle;
-
-    const index = this.props.todos.findIndex(todo => todo.id === id);
-    let payload = {
-      ourObject,
-      index
-    };
-    this.props.changeTitle(payload);
-  };
-  render() {
-    console.log(this.props.todos);
-    const todos = this.props.todos.map(todo => {
-      return (
-        <div key={todo.id} onClick={() => this.changeTitleFunction(todo.id)}>
-          {todo.title}
-        </div>
-      );
-    });
-    return <div>{todos}</div>;
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Todos} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-const mapStateToProps = (state, ownProps) => {
-  return state;
-};
-const mapDispatchToProps = dispatch => ({
-  changeTitle: payload => dispatch(changeTitle(payload))
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default App;
